12ddac9e2b04d2bbadedced46ca82950dc5aa82c
Nareg Keledjian <nkeledjian@gmail.com>
2019-08-01 19:59:07 -0700

    working out error in AddMessage container

--- src/containers/AddMessage.js
+++ src/containers/AddMessage.js
@@ -2,9 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const AddMessage = (props) => {
-    let input
+    let input;
 
-    return(
+    return (
         <section id="new-message"
             <input
                 onKeyPress = {(e) => {
@@ -16,13 +16,13 @@ const AddMessage = (props) => {
                 type = 'text'
                 ref {(node) => {
                     input = node
-                }}
+                }}>    
         </section>
     )
 }
 
 AddMessage.PropTypes = {
-    dispatch: PropTypes.func.isRequired;
+    dispatch: PropTypes.func.isRequired
 }
 
 export default AddMessage
---
+++ src/containers/Message.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const AddMessage = ({message, author}) => (
+    <p>
+        <i>{author}</i>: {message}
+    </p>
+    )
+
+AddMessage.PropTypes = {
+    message: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired
+}
+
+export default Message;
