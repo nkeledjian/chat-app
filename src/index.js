6aab85b4876647b1bd7ea08bc8cf17ceb21302f5
Nareg Keledjian <nkeledjian@gmail.com>
2020-06-10 18:56:17 -0700

    final touches, app seems to work locally

--- src/index.js
+++ src/index.js
@@ -2,15 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import createSagaMiddleware from 'redux-saga'; // redux-saga is a redux middleware
+import createSagaMiddleware from 'redux-saga'; 
+// redux-saga is a redux middleware which needs to be initialized during store creation
 
 // import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 import App from './App';
+// import { startWebsocket } from './actions';
 import setupSocket from './sockets';
+import reducers from './reducers';
 import handleNewMessage from './sagas'
 import username from './utils/name';
-import reducers from './reducers';
 
 const sagaMiddleware = createSagaMiddleware()
 
--- src/sagas/index.js
+++ src/sagas/index.js
@@ -5,7 +5,7 @@ import * as types from '../constants/ActionTypes'
 const handleNewMessage = function* handleNewMessage(params) {
     yield takeEvery(types.ADD_MESSAGE, (action) => {
         action.author = params.username
-        params.socket.send(JSON.stringify)
+        params.socket.send(JSON.stringify(action))
     })
 }
 
